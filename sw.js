c7215d3fee497c0f22c4c21885cc5a9d1d99c948
Parasbruce10 <parashamza955@gmail.com>
2026-04-21 01:53:44 +0500

    p

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10902557
+    "zoneId": 10903573
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
